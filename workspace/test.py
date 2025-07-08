26daaf03a088ecb4d100137e653e6153a877e86f
Megumi <111726120+Megumidesu@users.noreply.github.com>
2025-07-08 19:43:21 +0800

    Update test.py

--- workspace/test.py
+++ workspace/test.py
@@ -12,6 +12,7 @@ from CLIP.clip.model import *
 import torchvision.transforms as T
 from PIL import Image
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 
 set_seed(605)
 device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -44,20 +45,26 @@ def test_picture(img_path, checkpoint_path, ViT_model):
         normalize
     ])
 
-
+    print('*' * 50 + "加载数据" + '*' * 50)
     imgs=[]
     if not os.path.isfile(img_path):
-        img_list = os.listdir(img_path)
-        for i in img_list:
-            pil=Image.open(img_path + i)
-            pil = convert2jpg(pil)
-            imgs.append(transform(pil))
+        file_list = os.listdir(img_path)
+        img_list = []
+        for i in file_list:
+            if i.endswith('.png'):
+                pil=Image.open(os.path.join(img_path, i))
+                pil = convert2jpg(pil)
+                imgs.append(transform(pil))
+                img_list.append(os.path.join(img_path, i))
+        
     else:
         img_list = [img_path]
         pil=Image.open(img_path)
         pil = convert2jpg(pil)
         imgs.append(transform(pil))
     img_tensor=torch.stack(imgs).to(device)
+    print('*' * 10 + "数据加载完成" + '*' * 10)
+    print('\n' + '*' * 10 + "加载模型" + '*' * 10)
     model = TransformerClassifier(attr_num=5,attr_words=attr_words)
     model = model.to(device)
     model.eval()
@@ -65,6 +72,9 @@ def test_picture(img_path, checkpoint_path, ViT_model):
     ViT_model = ViT_model.to(device)
     checkpoint=torch.load(checkpoint_path)
     model.load_state_dict(checkpoint['model_state_dict'],strict=False)
+    print('*' * 10 + "模型加载完成" + '*' * 10)
+
+    print('\n' + '*' * 10 + "开始预测" + '*' * 10)    
     start = time.time()
     with torch.no_grad():
         logits = model(img_tensor, ViT_model=ViT_model)
@@ -72,22 +82,21 @@ def test_picture(img_path, checkpoint_path, ViT_model):
 
         index_list=[0, 5]
         group=['B-2', 'B-52H', 'E2D', 'F18', 'F35']
-        for idx in range(len(index_list)-1):
+        for idx in tqdm(range(len(index_list)-1), desc='模型预测'):
             if index_list[idx+1]-index_list[idx] >1 :
                 pred_result = np.argmax(probs, axis=1)
                 categories = [group[i] for i in pred_result]
-        print('--'*70+'\n')
-        for i, c in zip(img_list, categories):
-            print(f"img:{os.path.basename(i)}---------------------------------pred:{c}, label:{i.split('/')[-2]}")
-        print('\n'+'--'*70)
+        print('-'*10 + '预测结果' + '-'*10 + '\n')
+        for i, (img, cls) in enumerate(zip(img_list, categories)):
+            label = img.split('/')[-2]
+            print(f"sample:{i}------pred:{cls}, label:{label}, {True if cls==label else False}")
+        print('\n'+'-'*10)
     end = time.time()
     print(f"test time:{(end-start):.4f}s")
-        
-
+    
 if __name__ == '__main__':
     #要预测的图片路径或者文件夹路径
-    img_path = ''
+    img_path = 'test_imgs/B-52H'
     #模型权重文件路径
-    checkpoint_path = ''
+    checkpoint_path = '../vit_model/2024-08-25_23_28_48_epoch37_mf172.22.pth'
     test_picture(img_path, checkpoint_path, ViT_model)
-
